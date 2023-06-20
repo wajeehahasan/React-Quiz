d3a2457eda1e09f4dc78b4fd3fd76638eb683e42
Wajeeha Hasan <wajeehahasan2000@gmail.com>
2023-06-20 23:21:05 +0500

    Added initial components structure

---
+++ src/components/Question.js
@@ -0,0 +1,13 @@
+import Answer from "./Answer";
+const Question = () => {
+  return (
+    <div>
+      <div>Question</div>
+      <Answer />
+      <Answer />
+      <Answer />
+      <Answer />
+    </div>
+  );
+};
+export default Question;
---
+++ src/components/Quiz.js
@@ -0,0 +1,13 @@
+import Question from "./Question";
+const Quiz = () => {
+  return (
+    <div>
+      <div>
+        Quiz
+      </div>
+      <Question />
+      <Question />
+    </div>
+  );
+};
+export default Quiz;
